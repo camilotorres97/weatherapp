8f9b4c0bdaa866ab19a7bca7d727d9a104c6a9fa
Camilo Torres <juankmilo9740@hotmail.com>
2018-03-28 16:46:11 -0500

    Se mejora la independencia del componente trasladando todos los metodos de obtención de data a services/transformWeather.js y se maneja la obtención automatica al cargar la página usando la función componentWillMount para no quemar datos inicialmente, se remueve tambien el boton actualizar

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import { SUN } from './../../constants/weathers';
+import transformWeather from './../../services/transformWeather';
 import './styles.css';
-
-const data1 = {
-    temperature: 55,
-    weatherState: SUN,
-    humidity: 9,
-    wind: '10 m/s',
-};
 
 const location = "Bogota,co";
 const api_key = "e7fc755d2590a7ce3647260867c3b7a3";
@@ -21,33 +13,10 @@ class WeatherLocation extends Component {
     constructor() {
         super();
         this.state = {
-          city: 'Bogota',
-          data: data1  
+          city: 'Bogotá',
+          data: null  
         };
-    }
-
-    getTemp = kelvin => {
-        return convert(kelvin).from('K').to('C').toFixed(2);
-    }
-
-    getWeatherState = weather => {
-        return SUN;
-    }
-
-    getData = (weather_data) => {
-        const { humidity, temp } = weather_data.main;
-        const { speed } = weather_data.wind;
-        const weatherState = this.getWeatherState(this.weather);
-        const temperature = this.getTemp(temp);
-
-        const data = {
-            humidity,
-            temperature,
-            weatherState,
-            wind: `${speed}/ms`,
-        }
-
-        return data;
+        console.log("constructor");
     }
     
     handleUpdateClick = () => {
@@ -55,20 +24,24 @@ class WeatherLocation extends Component {
            console.log(data);
            return data.json();
        }).then( weather_data => {
-           debugger;
-           const data = this.getData(weather_data);
+           const data = transformWeather(weather_data);
            this.setState({ data });
        });
     }
 
+    componentWillMount() {
+        this.handleUpdateClick();
+    }
+
     render = () => {
+        console.log("render");
+
         const { city, data } = this.state;
 
         return(
         <div className="weatherLocationCont">
             <Location city={city} />
-            <WeatherData data={data} />
-            <button onClick={this.handleUpdateClick} >Actualizar</button>
+            {data ? <WeatherData data={data} /> : 'Cargando...'}
         </div>
         );
     };
